8f1c031df67f5229a943711e24cc2d6dc12ba40c
张祖全 <zhangsir1984@qq.com>
2024-01-11 16:11:39 +0800

    可以运行的代码

---
+++ samples/BookStore/src/BookStore.EntityFrameworkCore/Migrations/20240111080829_Created_Book_Entity3.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookStore.Migrations
+{
+    public partial class Created_Book_Entity3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
